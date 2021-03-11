2fb3f69b0774df0e8c88edeb810f1a3135d8e074
AN <skillzworldtech@gmail.com>
2021-03-11 19:20:18 +0530

    Modified SumDifference program to make the difference absolute

--- CProgramming/March-2021/8_SumDifference.c
+++ CProgramming/March-2021/8_SumDifference.c
@@ -7,7 +7,7 @@ int main() {
 
   long long int sum_of_squares = n * (n + 1) * (2 * n + 1) / 6,
     square_of_sum = pow(n * (n + 1) / 2, 2),
-    difference = square_of_sum - sum_of_squares;
+    difference = abs(square_of_sum - sum_of_squares);
 
   printf("%lld", difference);
   
