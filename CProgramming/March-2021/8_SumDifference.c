147b3044721a3adaf4c4c4331b84a43962fd732d
AN <skillzworldtech@gmail.com>
2021-03-11 19:40:34 +0530

    Changed 8_SumDifference.c code to make it more readable

--- CProgramming/March-2021/8_SumDifference.c
+++ CProgramming/March-2021/8_SumDifference.c
@@ -5,9 +5,9 @@
 int main() {
   int n = 100;
 
-  long long int sum_of_squares = n * (n + 1) * (2 * n + 1) / 6,
-    square_of_sum = pow(n * (n + 1) / 2, 2),
-    difference = abs(square_of_sum - sum_of_squares);
+  long long sum_of_squares = n * (n + 1) * (2 * n + 1) / 6;
+  long long square_of_sum = pow(n * (n + 1) / 2, 2);
+  long long difference = abs(square_of_sum - sum_of_squares);
 
   printf("%lld", difference);
   
