b668887806c4c5d45b45e3ed7e3cab150d94a1a0
AN <skillzworldtech@gmail.com>
2021-03-13 08:57:51 +0530

    Added another solution for input lines problem

---
+++ CProgramming/March-2021/10_PrintAllInputLines_Solution2.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_SIZE 1024
+
+int get_line(char buffer[]) {
+  fgets(buffer, MAX_SIZE, stdin);
+  
+  int len = strlen(buffer);
+  buffer[len - 1] = 0;
+
+  return len - 1;
+}
+
+int main() {
+  char buffer[MAX_SIZE];
+  int len;
+  const unsigned int limit = 20;
+
+  while((len = get_line(buffer)) > 0) {
+    if(len > limit) {
+      printf("%s\n", buffer);
+    }
+  }
+
+  return 0;
+}
