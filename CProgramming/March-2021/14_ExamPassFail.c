1886c7b3f5a79b9d35a10680076cdd08c4a5d6a0
AN <skillzworldtech@gmail.com>
2021-03-14 20:01:59 +0530

    Added comments to ExamPass.c program for better understanding of the code

--- CProgramming/March-2021/14_ExamPassFail.c
+++ CProgramming/March-2021/14_ExamPassFail.c
@@ -3,18 +3,27 @@
 #include <stdbool.h>
 
 int main() {
+  /* Constants that are necessary for making decisions */
   const int NUMBER_OF_SUBJECTS = 3;
   const int MIN_PER_SUBJECT_MARKS = 33;
   const int MIN_PASSING_PERCENTAGE = 40;
+
+  /* An array which stores the marks of all subjects */
+  float subject_marks[NUMBER_OF_SUBJECTS];
   
-  float subject_marks[NUMBER_OF_SUBJECTS], total_marks = 0;
+  /* A variable for storing total marks */
+  float total_marks = 0;
+
+  /* A flag which tells whether the student is passed or not */
   bool isPass = true;
 
+  /* Take the marks of all the subjects as input from the user */
   for(int i = 0; i < NUMBER_OF_SUBJECTS; ++i) {
     printf("Enter the marks for subject %d: ", i + 1);
     scanf("%f", &subject_marks[i]);
   }
 
+  /* Calculate total and check the conditions for passing */
   for(int i = 0; i < NUMBER_OF_SUBJECTS; ++i) {
     if(subject_marks[i] >= MIN_PER_SUBJECT_MARKS) {
       total_marks += subject_marks[i];
@@ -23,8 +32,11 @@ int main() {
       break;
     }
   }
+
+  /* Check whether total percentage is above minimum passing percentage */
   isPass = isPass && (total_marks / NUMBER_OF_SUBJECTS) >= MIN_PASSING_PERCENTAGE;
 
+  /* Display the result whether the student is passed or failed */
   if(isPass) {
     printf("You Passed the exam!\n");
   } else {
