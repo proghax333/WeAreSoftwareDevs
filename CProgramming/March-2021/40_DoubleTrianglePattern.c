9476e124cc148016d2b6fcd0ea8fa55b72db0111
AN <skillzworldtech@gmail.com>
2021-03-26 21:20:56 +0530

    Added double triangle pattern program in C

--- CProgramming/March-2021/40_DoubleTrianglePattern.c
+++ CProgramming/March-2021/40_DoubleTrianglePattern.c
@@ -7,6 +7,7 @@ int main()
   printf("Enter n: ");
   scanf("%d", &n);
 
+  // Upper triangle
   for (int i = 0; i < n; ++i)
   {
     for (int j = 0; j <= i; ++j)
@@ -16,6 +17,7 @@ int main()
     printf("\n");
   }
 
+  // Lower triangle
   for (int i = n - 2; i >= 0; --i)
   {
     for (int j = 0; j <= i; ++j)
