cacb8da04a1e86392a1b172232f288e4fbc4b202
AN <skillzworldtech@gmail.com>
2021-03-28 22:53:34 +0530

    Edit CentroidCalculator.cpp

--- Cpp/March-2021/CentroidCalculator.cpp
+++ Cpp/March-2021/CentroidCalculator.cpp
@@ -1,5 +1,8 @@
 
 #include <iostream>
+#include <tuple>
+#include <array>
+#include <cmath>
 
 namespace Math
 {
@@ -8,8 +11,8 @@ namespace Math
 
 using Number = long double;
 
-using Vector2 = std::tuple<Number, Number>;
-using Vector3 = std::tuple<Number, Number>;
+template <size_t _Size>
+using Vector = std::array<Number, _Size>;
 
 class Shape
 {
@@ -45,7 +48,29 @@ public:
   virtual Number getVolume() = 0;
 };
 
-class Square : public virtual Shape, AreaInterface
+template <size_t _Size>
+class Body : public virtual Shape
+{
+public:
+  virtual ~Body()
+  {
+  }
+  virtual Vector<_Size> getLocation()
+  {
+    return location;
+  }
+  virtual void setLocation(const Vector<_Size> &location)
+  {
+    this->location = location;
+  }
+
+  virtual Vector<2> getCentroid() = 0;
+
+protected:
+  Vector<_Size> location = {0, 0};
+};
+
+class Square : public virtual Shape, public AreaInterface, public virtual Body<2>
 {
 public:
   Square()
@@ -56,16 +81,22 @@ public:
   {
     this->setShapeType("square");
   }
-  Number getArea()
+
+  Number getArea() override
   {
     return this->sideLength * this->sideLength;
+  }
+  Vector<2> getCentroid() override
+  {
+    auto location = this->getLocation();
+    return Vector<2>({0, 0});
   }
 
 private:
   Number sideLength = 0.0;
 };
 
-class Circle : public virtual Shape, AreaInterface
+class Circle : public virtual Shape, public virtual AreaInterface, public virtual Body<2>
 {
 public:
   Circle()
@@ -77,9 +108,15 @@ public:
     this->setShapeType("circle");
   }
 
-  Number getArea()
+  Number getArea() override
   {
     return Math::PI * this->radius * this->radius;
+  }
+
+  Vector<2> getCentroid() override
+  {
+    auto location = this->getLocation();
+    return Vector<2>({location[0], location[1]});
   }
 
 private:
@@ -88,6 +125,13 @@ private:
 
 int main()
 {
+  Body<2> *circleBody = new Circle(2);
+  circleBody->setLocation({10, 10});
+
+  auto circleBodyCentroid = circleBody->getCentroid();
+
+  std::cout << "Cx: " << circleBodyCentroid[0] << std::endl
+            << "Cy: " << circleBodyCentroid[1] << std::endl;
 
   return 0;
 }
