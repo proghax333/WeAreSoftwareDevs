e7e5c211f6477603a7e5d67bd04b3cadfb47f769
AN <skillzworldtech@gmail.com>
2021-04-01 00:20:03 +0530

    Fix typo in CircleProperties.hpp

--- Cpp/March-2021/CentroidCalculator/properties/CircleProperties.hpp
+++ Cpp/March-2021/CentroidCalculator/properties/CircleProperties.hpp
@@ -10,7 +10,7 @@ class CircleProperties : public virtual Shape
 public:
   CircleProperties()
   {
-    // Add angle property to the shape
+    // Add radius property to the shape
     properties["radius"] = Property{.type = "shapeProperty", .value = Number(0.0)};
   }
   ~CircleProperties()
